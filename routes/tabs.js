3c4543569aa348b69e72ccd98f61486c663c3d05
wangjie <pudge_wj@163.com>
2020-01-28 21:40:59 +0800

    跨域

--- routes/tabs.js
+++ routes/tabs.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.json({
     status: '0',
     msg: '',
