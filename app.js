2baebb6f92ae8ce228aa733a8ffd7a7ad091756d
wangjie <pudge_wj@163.com>
2020-01-28 21:14:42 +0800

    添加了首页tab

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var lunboRouter = require('./routes/lunbo');
+var tabRouter = require('./routes/tabs')
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/lunbo', lunboRouter);
+app.use('/tabs', tabRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
